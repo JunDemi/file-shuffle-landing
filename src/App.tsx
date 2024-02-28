9dae31fc8face44d2ad4b4970d48866b75a7f4c2
JunDemi <jungwook3176@gmail.com>
2024-02-28 14:38:43 +0900

    메인

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
 import Header from './components/Header';
 import './asset/Main.scss';
+import ScrollTopButton from './components/ScrollTopButton';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <ScrollTopButton/>
     </BrowserRouter>
   );
 }
